94ae9164d088e2244fc9bf69351d8edd10464ca5
Uislan Cruz <uislansk8@hotmail.com>
2025-06-03 21:40:28 -0300

    Formas diferentes de percorrer o arrays usando o Fori e ForE

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,9 +2,15 @@ public class Calculadora {
     static double calculaMedia(int[] numeros){
         int total = 0;
 
-        for (int i = 0; i < numeros.length; i++){
-            total += numeros[i];
+        //aqui voce deixa mais inteirado caso for até o final do indice.
+        for (int numero : numeros) {
+            total += numero;
         }
+
+        //caso queira para sem ir ao final do indice
+//        for (int i = 0; i < numeros.length; i++){
+//            total += numeros[i];
+//        }
         return (double) total / numeros.length;
     }
 }
--- src/Principal2.java
+++ src/Principal2.java
@@ -1,6 +1,6 @@
 public class Principal2 {
     public static void main(String[] args) {
-        int[] notas = new int[]{8, 5 , 4, 10,};
+        int[] notas = new int[]{8, 5 , 4, 9, 10,};
 
         double media = Calculadora.calculaMedia(notas);
 
