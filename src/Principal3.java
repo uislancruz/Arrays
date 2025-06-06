2cbc707cd39da9cdeadf9471a4098cd3bd7dbb0a
Uislan Cruz <uislansk8@hotmail.com>
2025-06-06 20:53:18 -0300

    Ordenando arrays na ordem crescente (sort) e na decrescente (sort(Comparator.reverseOrder())

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Principal3 {
 
     public static void main(String[] args) {
-        int[] notas = {8, 5, 4, 10, 9};
+        Integer[] notas = {8, 5, 4, 10, 9};
 
-        Arrays.sort(notas);
+        Arrays.sort(notas, Comparator.reverseOrder());
 
         System.out.println(Arrays.toString(notas));
 
