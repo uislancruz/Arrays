60e10e93871d0797185101835ff7d2cd4aa1b746
Uislan Cruz <uislansk8@hotmail.com>
2025-06-06 20:22:24 -0300

    Usando o toString

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -8,7 +11,9 @@ public class Principal {
         //ou assim
         //int[] notas = {8, 5 , 4, 10,};
 
-        System.out.println(notas[1]);
+       String notasEmString = Arrays.toString(notas);
+
+        System.out.println(notasEmString);
 
 
     }
